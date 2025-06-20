033464de26cfddf0abf6903743fbaa4762cebdd6
ahsadhik <ahsadhik007@gmail.com>
2025-06-20 07:24:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-/*
 
-GEREKLİ PAKETLER YÜKLENİYOR...
-
-*/
 var http = require('http');
 var express = require('express');
 
@@ -12,6 +8,11 @@ app.set('port', process.env.PORT || 3005); // GİRİŞ PORTU AYARLANDI
 app.set('views', __dirname + '/app/server/views'); // VIEW KLASÖRÜ TANITILDI
 app.set('view engine', 'ejs'); // VIEW ENGINE AYARLANDI
 app.use(express.static(__dirname + '/app/public')); // KULLANICILAR TARAFINDAN ERİŞİLEBİLEN KLASÖR TANIMLANDI
+
+// ✅ ADD THIS LINE
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
 
 require('./app/routes')(app); // ROUTE DOSYASI ÇAĞIRILDI
 
